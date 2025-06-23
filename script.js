4dac9ad9e44bebabbb5df64766805c583425fa6b
safaltars <safaltars319@gmail.com>
2025-06-23 15:19:11 +0545

    Let's see

---
+++ script.js
@@ -0,0 +1,7 @@
+function greet(){
+ let divContent = document.getElementsByClassName("jstutor");
+ divContent[0].style.backgroundColor = "red"
+ divContent[1].style.backgroundColor = "pink"
+ divContent[2].style.backgroundColor = "black"
+ divContent[3].style.backgroundColor = "blue" 
+}
